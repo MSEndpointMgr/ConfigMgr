8e1a8c92886428d7451abbfd7fd6087a75218e8f
maurice-daly <maurice.daly@outlook.com>
2017-10-22 00:33:47 +0100

    Update Invoke-CMDriverMaintenance.ps1

--- Operating System Deployment/Drivers/Invoke-CMDriverMaintenance.ps1
+++ Operating System Deployment/Drivers/Invoke-CMDriverMaintenance.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .SYNOPSIS
 	Refresh Driver script for Windows
 	
@@ -11,28 +11,38 @@
     Author:      Maurice Daly
     Contact:     @MoDaly_IT
     Created:     2017-10-13
-	Updated:     2017-10-13
-	
+	Updated:     2017-10-22
+
 	Minimum required version of ConfigMgr WebService: 1.4.0
     
     Version history:
     1.0.0 - (2017-10-13) Script created
+	1.0.1 - (2017-10-22) Variable placement
 #>
+
+# Define Variables
+$TaskSequenceBase = Join-Path -Path (Get-WmiObject Win32_OperatingSystem).SystemDrive -ChildPath "_SMSTaskSequence"
+$LogFilePath = Join-Path -Path (Get-WmiObject Win32_OperatingSystem).SystemDrive -ChildPath "Windows\Logs"
+$DriverPackagePath = Join-Path -Path $TaskSequenceBase -ChildPath "DriverPackage"
+
 function Write-CMLogEntry {
 	param (
 		[parameter(Mandatory = $true, HelpMessage = "Value added to the log file.")]
 		[ValidateNotNullOrEmpty()]
-		[string]$Value,
+		[string]
+		$Value,
 		[parameter(Mandatory = $true, HelpMessage = "Severity for the log entry. 1 for Informational, 2 for Warning and 3 for Error.")]
 		[ValidateNotNullOrEmpty()]
 		[ValidateSet("1", "2", "3")]
-		[string]$Severity,
+		[string]
+		$Severity,
 		[parameter(Mandatory = $false, HelpMessage = "Name of the log file that the entry will written to.")]
 		[ValidateNotNullOrEmpty()]
-		[string]$FileName = "ApplyDriverMaintenancePackage.log"
+		[string]
+		$FileName = "ApplyDriverMaintenancePackage.log"
 	)
 	# Determine log file location
-	$LogFilePath = Join-Path -Path C:\Windows\Temp -ChildPath $FileName
+	$LogFilePath = Join-Path -Path $script:LogFilePath -ChildPath $FileName
 	
 	# Construct time stamp for log entry
 	$Time = -join @((Get-Date -Format "HH:mm:ss.fff"), "+", (Get-WmiObject -Class Win32_TimeZone | Select-Object -ExpandProperty Bias))
@@ -49,8 +59,7 @@ function Write-CMLogEntry {
 	# Add value to log file
 	try {
 		Add-Content -Value $LogText -LiteralPath $LogFilePath -ErrorAction Stop
-	}
-	catch [System.Exception] {
+	} catch [System.Exception] {
 		Write-Warning -Message "Unable to append log entry to ApplyDriverMaintenancePackage.log file. Error message: $($_.Exception.Message)"
 	}
 }
@@ -58,11 +67,11 @@ function Write-CMLogEntry {
 # Apply driver maintenance package
 try {
 	Write-CMLogEntry -Value "Starting driver installation process" -Severity 1
-	Get-ChildItem -Path "C:\_SMSTaskSequence\DriverPackage" -Filter *.inf -Recurse | ForEach-Object {
+	Write-CMLogEntry -Value "Reading drivers from $DriverPackagePath" -Severity 1
+	Get-ChildItem -Path $DriverPackagePath -Filter *.inf -Recurse | ForEach-Object {
 		pnputil /add-driver $_.FullName /install
-	} | Out-File -FilePath C:\Windows\Temp\DriverMaintenance.log -Force
+	} | Out-File -FilePath (Join-Path -Path $LogFilePath -ChildPath DriverMaintenance.log) -Force
 	Write-CMLogEntry -Value "Driver installation complete. Restart required" -Severity 1
-}
-catch [System.Exception] {
+} catch [System.Exception] {
 	Write-CMLogEntry -Value "An error occurred while attempting to apply the driver maintenance package. Error message: $($_.Exception.Message)" -Severity 3
 }
