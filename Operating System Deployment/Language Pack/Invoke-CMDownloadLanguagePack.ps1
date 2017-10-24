bc19ca83e5477551c77868b94a3da8e4611fa8ad
Nickolaj Andersen <nickolaj@outlook.com>
2017-10-24 08:09:54 +0200

    Version 1.0.1

--- Operating System Deployment/Language Pack/Invoke-CMDownloadLanguagePack.ps1
+++ Operating System Deployment/Language Pack/Invoke-CMDownloadLanguagePack.ps1
@@ -108,6 +108,42 @@ Process {
         }
     }
 
+    function Invoke-Executable {
+		param (
+			[parameter(Mandatory = $true, HelpMessage = "Specify the file name or path of the executable to be invoked, including the extension")]
+			[ValidateNotNullOrEmpty()]
+			[string]$FilePath,
+			[parameter(Mandatory = $false, HelpMessage = "Specify arguments that will be passed to the executable")]
+			[ValidateNotNull()]
+			[string]$Arguments
+		)
+		
+		# Construct a hash-table for default parameter splatting
+		$SplatArgs = @{
+			FilePath	 = $FilePath
+			NoNewWindow  = $true
+			Passthru	 = $true
+			ErrorAction  = "Stop"
+		}
+		
+		# Add ArgumentList param if present
+		if (-not ([System.String]::IsNullOrEmpty($Arguments))) {
+			$SplatArgs.Add("ArgumentList", $Arguments)
+		}
+		
+		# Invoke executable and wait for process to exit
+		try {
+			$Invocation = Start-Process @SplatArgs
+			$Handle = $Invocation.Handle
+			$Invocation.WaitForExit()
+		}
+		catch [System.Exception] {
+			Write-Warning -Message $_.Exception.Message; break
+		}
+		
+		return $Invocation.ExitCode
+	}
+
 	function Invoke-CMDownloadContent {
 		param (
 			[parameter(Mandatory = $true, ParameterSetName = "NoPath", HelpMessage = "Specify a PackageID that will be downloaded.")]
@@ -152,7 +188,7 @@ Process {
 		# Invoke download of package content
 		try {
 			Write-CMLogEntry -Value "Starting package content download process, this might take some time" -Severity 1
-			$ReturnCode = Invoke-Executable -FilePath "OSDDownloadContent.exe"
+			$ReturnCode = Invoke-Executable -FilePath "C:\Windows\CCM\OSDDownloadContent.exe"
 
 			# Match on return code
 			if ($ReturnCode -eq 0) {
