7419b86c39cc2d292a10859b0a53bdbf3ad11811
maurice-daly <maurice.daly@outlook.com>
2017-11-07 15:57:51 +0000

    Update Invoke-CMApplyDriverPackage.ps1

--- Operating System Deployment/Drivers/Invoke-CMApplyDriverPackage.ps1
+++ Operating System Deployment/Drivers/Invoke-CMApplyDriverPackage.ps1
@@ -23,13 +23,16 @@
 
 	# Detect and download drivers during OS upgrade with ConfigMgr:
 	.\Invoke-CMApplyDriverPackage.ps1 -URI "http://CM01.domain.com/ConfigMgrWebService/ConfigMgr.asmx" -SecretKey "12345" -Filter "Drivers" -OSMaintenance
+
+	# Detect the OS and use a driver fallback package with ConfigMgr:
+	.\Invoke-CMApplyDriverPackage.ps1 -URI "http://CM01.domain.com/ConfigMgrWebService/ConfigMgr.asmx" -SecretKey "12345" -Filter "Drivers" -DriverFallback
 	
 .NOTES
     FileName:    Invoke-CMApplyDriverPackage.ps1
     Author:      Nickolaj Andersen / Maurice Daly
     Contact:     @NickolajA / @MoDaly_IT
     Created:     2017-03-27
-	Updated:     2017-10-29
+	Updated:     2017-11-07
 	
 	Minimum required version of ConfigMgr WebService: 1.4.0
     
@@ -44,32 +47,36 @@
     1.0.7 - (2017-05-26) Updated script to filter OS when multiple model matches are found for different OS platforms
     1.0.8 - (2017-06-26) Updated script with improved computer name matching when filtering out packages returned from the web service
     1.0.9 - (2017-08-25) Updated script to read package description for Microsoft models in order to match the WMI value contained within
-    1.1.0 - (2017-08-29) Updated script to only check for the OS build version instead of major, minor, build and revision for HP systems
-						 $OSImageVersion will now only contain the most recent version if multiple OS images is referenced in the Task Sequence
-	1.1.1 - (2017-09-12) Updated script to match the system SKU for Dell, Lenovo and HP models. Added architecture check for matching packages
-	1.1.2 - (2017-09-15) Replaced computer model matching with SystemSKU. Added script with support for different exit codes
-	1.1.3 - (2017-09-18) Added support for downloading package content instead of setting OSDDownloadDownloadPackages variable
-	1.1.4 - (2017-09-19) Added support for installing driver package directly from this script instead of running a seperate DISM command line step
-	1.1.5 - (2017-10-12) Added support for in full OS driver maintaince updates
-	1.1.6 - (2017-10-29) Fixed an issue when detecting Microsoft manufacturer information
-	1.1.7 - (2017-10-29) Changed the OSMainteance parameter from a string to a switch object, make sure that your implementation of this is amended in any task sequence steps
+    1.1.0 - (2017-08-29) Updated script to only check for the OS build version instead of major, minor, build and revision for HP systems. $OSImageVersion will now only contain the most recent version if multiple OS images is referenced in the Task Sequence
+    1.1.1 - (2017-09-12) Updated script to match the system SKU for Dell, Lenovo and HP models. Added architecture check for matching packages
+    1.1.2 - (2017-09-15) Replaced computer model matching with SystemSKU. Added script with support for different exit codes
+    1.1.3 - (2017-09-18) Added support for downloading package content instead of setting OSDDownloadDownloadPackages variable
+    1.1.4 - (2017-09-19) Added support for installing driver package directly from this script instead of running a seperate DISM command line step
+    1.1.5 - (2017-10-12) Added support for in full OS driver maintaince updates
+    1.1.6 - (2017-10-29) Fixed an issue when detecting Microsoft manufacturer information
+    1.1.7 - (2017-10-29) Changed the OSMainteance parameter from a string to a switch object, make sure that your implementation of this is amended in any task sequence steps
+    1.1.8 - (2017-11-07) Added support for driver fallback packages when the UseDriverFallback param is used
 #>
-[CmdletBinding(SupportsShouldProcess=$true)]
+[CmdletBinding(SupportsShouldProcess = $true)]
 param (
-	[parameter(Mandatory=$true, HelpMessage="Set the URI for the ConfigMgr WebService.")]
+	[parameter(Mandatory = $true, HelpMessage = "Set the URI for the ConfigMgr WebService.")]
 	[ValidateNotNullOrEmpty()]
-	[string]$URI,
-
-	[parameter(Mandatory=$true, HelpMessage="Specify the known secret key for the ConfigMgr WebService.")]
+	[string]
+	$URI,
+	[parameter(Mandatory = $true, HelpMessage = "Specify the known secret key for the ConfigMgr WebService.")]
 	[ValidateNotNullOrEmpty()]
-	[string]$SecretKey,
-
-	[parameter(Mandatory=$false, HelpMessage="Define a filter used when calling ConfigMgr WebService to only return objects matching the filter.")]
+	[string]
+	$SecretKey,
+	[parameter(Mandatory = $false, HelpMessage = "Define a filter used when calling ConfigMgr WebService to only return objects matching the filter.")]
 	[ValidateNotNullOrEmpty()]
-	[string]$Filter = ([System.String]::Empty),
-
-	[parameter(Mandatory=$false, HelpMessage="Specify if the script is to be used as part of an in-OS maintenance task")]
-	[switch]$OSMaintenance
+	[string]
+	$Filter = ([System.String]::Empty),
+	[parameter(Mandatory = $false, HelpMessage = "Specify if the script is to be used as part of an in-OS maintenance task")]
+	[switch]
+	$OSMaintenance,
+	[parameter(Mandatory = $false, HelpMessage = "Specify if the script is to be used with a driver fallback package")]
+	[switch]
+	$UseDriverFallback
 )
 Begin {
 	# Load Microsoft.SMS.TSEnvironment COM object
@@ -86,14 +93,17 @@ Process {
 		param (
 			[parameter(Mandatory = $true, HelpMessage = "Value added to the log file.")]
 			[ValidateNotNullOrEmpty()]
-			[string]$Value,
+			[string]
+			$Value,
 			[parameter(Mandatory = $true, HelpMessage = "Severity for the log entry. 1 for Informational, 2 for Warning and 3 for Error.")]
 			[ValidateNotNullOrEmpty()]
 			[ValidateSet("1", "2", "3")]
-			[string]$Severity,
+			[string]
+			$Severity,
 			[parameter(Mandatory = $false, HelpMessage = "Name of the log file that the entry will written to.")]
 			[ValidateNotNullOrEmpty()]
-			[string]$FileName = "ApplyDriverPackage.log"
+			[string]
+			$FileName = "ApplyDriverPackage.log"
 		)
 		# Determine log file location
 		$LogFilePath = Join-Path -Path $Script:TSEnvironment.Value("_SMSTSLogPath") -ChildPath $FileName
@@ -123,18 +133,20 @@ Process {
 		param (
 			[parameter(Mandatory = $true, HelpMessage = "Specify the file name or path of the executable to be invoked, including the extension")]
 			[ValidateNotNullOrEmpty()]
-			[string]$FilePath,
+			[string]
+			$FilePath,
 			[parameter(Mandatory = $false, HelpMessage = "Specify arguments that will be passed to the executable")]
 			[ValidateNotNull()]
-			[string]$Arguments
+			[string]
+			$Arguments
 		)
 		
 		# Construct a hash-table for default parameter splatting
 		$SplatArgs = @{
-			FilePath = $FilePath
-			NoNewWindow = $true
-			Passthru = $true
-			ErrorAction = "Stop"
+			FilePath	   = $FilePath
+			NoNewWindow    = $true
+			Passthru	   = $true
+			ErrorAction    = "Stop"
 		}
 		
 		# Add ArgumentList param if present
@@ -161,19 +173,23 @@ Process {
 			[Parameter(ParameterSetName = "CustomPath")]
 			[ValidateNotNullOrEmpty()]
 			[ValidatePattern("^[A-Z0-9]{3}[A-F0-9]{5}$")]
-			[string]$PackageID,
+			[string]
+			$PackageID,
 			[parameter(Mandatory = $true, ParameterSetName = "NoPath", HelpMessage = "Specify the download location type.")]
 			[Parameter(ParameterSetName = "CustomPath")]
 			[ValidateNotNullOrEmpty()]
 			[ValidateSet("Custom", "TSCache", "CCMCache")]
-			[string]$DestinationLocationType,
+			[string]
+			$DestinationLocationType,
 			[parameter(Mandatory = $true, ParameterSetName = "NoPath", HelpMessage = "Save the download location to the specified variable name.")]
 			[Parameter(ParameterSetName = "CustomPath")]
 			[ValidateNotNullOrEmpty()]
-			[string]$DestinationVariableName,
+			[string]
+			$DestinationVariableName,
 			[parameter(Mandatory = $true, ParameterSetName = "CustomPath", HelpMessage = "When location type is specified as Custom, specify the custom path.")]
 			[ValidateNotNullOrEmpty()]
-			[string]$CustomLocationPath
+			[string]
+			$CustomLocationPath
 		)
 		# Set OSDDownloadDownloadPackages
 		Write-CMLogEntry -Value "Setting task sequence variable OSDDownloadDownloadPackages to: $($PackageID)" -Severity 1
@@ -235,7 +251,8 @@ Process {
 		param (
 			[parameter(Mandatory = $true, HelpMessage = "Windows build version must be provided")]
 			[ValidateNotNullOrEmpty()]
-			[string]$OSImageVersion
+			[string]
+			$OSImageVersion
 		)
 		
 		# Get operating system name from version
@@ -423,7 +440,7 @@ Process {
 							# Attempt to download driver package content
 							Write-CMLogEntry -Value "Driver package list contains a single match, attempting to download driver package content" -Severity 1
 							$DownloadInvocation = Invoke-CMDownloadContent -PackageID $PackageList[0].PackageID -DestinationLocationType Custom -DestinationVariableName "OSDDriverPackage" -CustomLocationPath "%_SMSTSMDataPath%\DriverPackage"
-		
+							
 							try {
 								if ($DownloadInvocation -eq 0) {
 									if ($PSBoundParameters.ContainsKey("OSMaintenance") -eq $false) {
@@ -500,16 +517,60 @@ Process {
 						Write-CMLogEntry -Value "Unable to determine a matching driver package from package list array, unhandled amount of matches" -Severity 2; exit 7
 					}
 				}
+				elseif ($PSBoundParameters.ContainsKey("UseDriverFallback") -eq $true) {
+					Write-CMLogEntry -Value "Driver fallback parameter specified and no matching driver packages found." -Severity 1
+					# Process packages returned from web service
+					if ($Packages -ne $null) {
+						Write-CMLogEntry -Value "Attempting to match a driver fallback package" -Severity 1
+						foreach ($Package in $Packages) {
+							Write-CMLogEntry -Value "Processing $($Package.PackageName)" -Severity 1
+							if (($Package.PackageName -match "Driver Fallback") -and ($Package.PackageName -match $OSName) -and ($Package.PackageName -match $OSImageArchitecture)) {
+								$PackageList.Add($Package) | Out-Null
+								try {
+									# Attempt to download driver fallback package content
+									Write-CMLogEntry -Value "Attempting to download driver fallback package content" -Severity 1
+									$DownloadInvocation = Invoke-CMDownloadContent -PackageID $PackageList[0].PackageID -DestinationLocationType Custom -DestinationVariableName "OSDDriverPackage" -CustomLocationPath "%_SMSTSMDataPath%\DriverPackage"
+									
+									try {
+										if ($DownloadInvocation -eq 0) {
+											if ($PSBoundParameters.ContainsKey("OSMaintenance") -eq $false) {
+												# Apply drivers recursively from downloaded driver package location
+												Write-CMLogEntry -Value "Driver fallback package content downloaded successfully, attempting to apply drivers using dism.exe located in: $($TSEnvironment.Value('OSDDriverPackage01'))" -Severity 1
+												$ApplyDriverInvocation = Invoke-Executable -FilePath "Dism.exe" -Arguments "/Image:$($TSEnvironment.Value('OSDisk'))\ /Add-Driver /Driver:$($TSEnvironment.Value('OSDDriverPackage01')) /Recurse"
+												# Validate driver injection
+												if ($ApplyDriverInvocation -eq 0) {
+													Write-CMLogEntry -Value "Successfully applied drivers using dism.exe" -Severity 1
+												}
+												else {
+													Write-CMLogEntry -Value "An error occurred while applying drivers (single package match). Exit code: $($ApplyDriverInvocation)" -Severity 3; exit 14
+												}
+											}
+										}
+										else {
+											Write-CMLogEntry -Value "Driver package content download process returned an unhandled exit code: $($DownloadInvocation)" -Severity 3; exit 13
+										}
+									}
+									catch [System.Exception] {
+										Write-CMLogEntry -Value "An error occurred while applying drivers (single package match). Error message: $($_.Exception.Message)" -Severity 3; exit 14
+									}
+								}
+								catch [System.Exception] {
+									Write-CMLogEntry -Value "An error occurred while downloading driver package content (single package match). Error message: $($_.Exception.Message)" -Severity 3; exit 5
+								}
+							}
+						}
+					}
+					else {
+						Write-CMLogEntry -Value "Empty driver package list detected, unable to determine matching driver fallback package" -Severity 2; exit 8
+					}
+				}
 				else {
-					Write-CMLogEntry -Value "Empty driver package list detected, unable to determine matching driver package" -Severity 2; exit 8
+					Write-CMLogEntry -Value "Call to web service for package objects returned empty" -Severity 2; exit 9
 				}
 			}
 			else {
-				Write-CMLogEntry -Value "Call to web service for package objects returned empty" -Severity 2; exit 9
+				Write-CMLogEntry -Value "Unsupported computer platform detected, virtual machines are not supported" -Severity 2; exit 10
 			}
-		}
-		else {
-			Write-CMLogEntry -Value "Unsupported computer platform detected, virtual machines are not supported" -Severity 2; exit 10
 		}
 	}
 	else {
