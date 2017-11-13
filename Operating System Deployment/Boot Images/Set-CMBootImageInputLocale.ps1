5682aa701ba492def7c6101ad437408f3113671f
Nickolaj Andersen <nickolaj@outlook.com>
2017-11-13 16:11:06 +0100

    Version 1.0.0

--- Operating System Deployment/Boot Images/Set-CMBootImageInputLocale.ps1
+++ Operating System Deployment/Boot Images/Set-CMBootImageInputLocale.ps1
@@ -1,19 +1,18 @@
-﻿<#
+<#
 .SYNOPSIS
-    Add a hotfix to Boot Images in Configuration Manager.
+    Set the InputLocale property for a Boot Image in Configuration Manager.
 
 .DESCRIPTION
-    This script will apply a hotfix (.msu file) to any number of given Boot Images in Configuration identified by their PackageID property.
-    For running this script, it's required to be executed on a Primary Site server where
+    Set the InputLocale property for a Boot Image in Configuration Manager.
 
 .PARAMETER SiteServer
     Site Server where the SMS Provider is installed.
 
 .PARAMETER PackageID
-    Specify a string or a string array of Boot Image PackageID's.
+    Specify the PackageID property of a Boot Image.
 
-.PARAMETER Path
-    Specify the full path to the hotfix that should be applied to the given Boot Image.
+.PARAMETER Locale
+    Specify the input locales that should be configured for this selected Boot Image, e.g. 'en-US' and 'sv-SE'.
 
 .PARAMETER MountPath
     Specify the full path for a temporary mount location. Should be an existing empty folder.
@@ -22,24 +21,18 @@
     Invoke a package source update of the affected Boot Image(s).
 
 .EXAMPLE
-    # Apply a hotfix called 'windows10.0-kb4025632-x64.msu' to a Boot Image with PackageID 'P01000AB':
-    .\Add-CMBootImageHotfix.ps1 -SiteServer CM01 -PackageID "P01000AB" -Path "C:\Temp\windows10.0-kb4025632-x64.msu" -MountPath "C:\Temp\Mount" -Verbose
-
-    # Apply a hotfix called 'windows10.0-kb4025632-x64.msu' to multiple Boot Images with PackageID's 'P01000AB' and 'P01000BA':
-    .\Add-CMBootImageHotfix.ps1 -SiteServer CM01 -PackageID "P01000AB", "P01000BA" -Path "C:\Temp\windows10.0-kb4025632-x64.msu" -MountPath "C:\Temp\Mount" -Verbose
-
-    # Apply a hotfix called 'windows10.0-kb4025632-x64.msu' to a Boot Image with PackageID 'P01000AB' and refresh the Boot Image package with the changes made:
-    .\Add-CMBootImageHotfix.ps1 -SiteServer CM01 -PackageID "P01000AB" -Path "C:\Temp\windows10.0-kb4025632-x64.msu" -MountPath "C:\Temp\Mount" -RefreshPackage -Verbose    
+    # Set the input locale to 'sv-SE' and 'en-US' for a Boot Image with PackageID 'P01000AB':
+    .\Set-CMBootImageInputLocale.ps1 -SiteServer CM01 -PackageID "P01000AB" -Locale "sv-SE","en-US" -MountPath "C:\Temp\Mount" -Verbose
 
 .NOTES
-    FileName:    Add-CMBootImageHotfix.ps1
+    FileName:    Set-CMBootImageInputLocale.ps1
     Author:      Nickolaj Andersen
     Contact:     @NickolajA
-    Created:     2017-08-27
-    Updated:     2017-08-27
+    Created:     2017-11-13
+    Updated:     2017-11-13
     
     Version history:
-    1.0.0 - (2017-08-27) Script created
+    1.0.0 - (2017-11-13) Script created
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
 param(
@@ -48,30 +41,15 @@ param(
     [ValidateScript({Test-Connection -ComputerName $_ -Count 1 -Quiet})]
     [string]$SiteServer,
 
-    [parameter(Mandatory=$true, HelpMessage="Specify a string or a string array of Boot Image PackageID's.")]
+    [parameter(Mandatory=$true, HelpMessage="Specify the PackageID property of a Boot Image.")]
     [ValidatePattern("^[A-Z0-9]{3}[A-F0-9]{5}$")]
     [ValidateNotNullOrEmpty()]
-    [string[]]$PackageID,
+    [string]$PackageID,
 
-    [parameter(Mandatory=$true, HelpMessage="Specify the full path to the hotfix (.msu file) that should be applied to the given Boot Image.")]
+    [parameter(Mandatory=$true, HelpMessage="Specify the input locales that should be configured for this selected Boot Image, e.g. 'en-US' and 'sv-SE'.")]
     [ValidateNotNullOrEmpty()]
-    [ValidatePattern("^[A-Za-z]{1}:\\\w+\\\w+")]
-    [ValidateScript({
-	    # Check if path contains any invalid characters
-	    if ((Split-Path -Path $_ -Leaf).IndexOfAny([IO.Path]::GetInvalidFileNameChars()) -ge 0) {
-		    Write-Warning -Message "$(Split-Path -Path $_ -Leaf) contains invalid characters" ; break
-	    }
-	    else {
-	        # Check if file extension is MSU
-		    if ([System.IO.Path]::GetExtension((Split-Path -Path $_ -Leaf)) -like ".msu") {
-			    return $true
-		    }
-		    else {
-			    Write-Warning -Message "$(Split-Path -Path $_ -Leaf) contains unsupported file extension. Supported extension is '.msu'" ; break
-		    }
-	    }
-    })]
-    [string]$Path,
+    [ValidateSet("ar-sa", "bg-bg", "cs-cz", "da-dk", "de-de", "el-gr", "en-gb", "en-us", "es-es", "es-mx", "et-ee", "fi-fi", "fr-ca", "fr-fr", "he-il", "hr-hr", "hu-hu", "it-it", "ja-jp", "ko-kr", "lt-lt", "lv-lv", "nb-no", "nl-nl", "pl-pl", "pt-br", "pt-pt", "ro-ro", "ru-ru", "sk-sk", "sl-si", "sr-latn-rs", "sv-se", "th-th", "tr-tr", "uk-ua", "zh-cn", "zh-tw")]
+    [string[]]$Locale,
 
     [parameter(Mandatory=$true, HelpMessage="Specify the full path for a temporary mount location. Should be an existing empty folder.")]
     [ValidatePattern("^[A-Za-z]{1}:\\\w+\\\w+")]
@@ -94,7 +72,7 @@ param(
 		    }
 	    }
     })]
-    [string]$MountPath,
+    [string]$MountPath,    
 
     [parameter(Mandatory=$false, HelpMessage="Invoke a package source update of the affected Boot Image(s).")]
     [switch]$UpdatePackage
@@ -201,23 +179,29 @@ Process {
                         # Mount Boot Image to mount directory
                         Write-Verbose -Message "Attempting to mount '$($BootImagePackage.Name)' from: $($BootImagePath)"
 
-                        $ReturnValue = Invoke-Executable -FilePath $DeploymentToolsDISMPath -Arguments "/Mount-Image /ImageFile:""$($ImagePath)"" /Index:1 /MountDir:""$($MountPath)"""
+                        $ReturnValue = Invoke-Executable -FilePath $DeploymentToolsDISMPath -Arguments "/Mount-Image /ImageFile:""$($BootImagePath)"" /Index:1 /MountDir:""$($MountPath)"""
                         if ($ReturnValue -eq 0) {
                             Write-Verbose -Message "Successfully mounted '$($BootImagePackage.Name)'"
 
                             try {
-                                # Inject hotfix into offline image
-                                Write-Verbose -Message "Attempting to inject hotfix into Boot Image mount location: $($MountPath)"
-                                $InjectPackage = Add-WindowsPackage -PackagePath $Path -Path $MountPath -ErrorAction Stop -Verbose:$false
-                                if ($InjectPackage -ne $null) {
-                                    Write-Verbose -Message "Successfully injected hotfix into mounted Boot Image"
+                                # Combine list of locales
+                                if ($Locale.Count -ge 2) {
+                                    $Locales = $Locale -join ";"
+                                }
+
+                                # Set InputLocale for Boot Image
+                                Write-Verbose -Message "Attempting to set input locale for mounted Boot Image: $($BootImagePath)"
+                                $LocaleInvocation = Invoke-Executable -FilePath $DeploymentToolsDISMPath -Arguments "/Image:""$($MountPath)"" /Set-InputLocale:$($Locales)"
+                                if ($LocaleInvocation -eq 0) {
+                                    Write-Verbose -Message "Successfully set input locale for mounted Boot Image: $($BootImagePath)"
                                 }
                             }
                             catch [System.Exception] {
-                                Write-Warning -Message "An error occurred while injecting package in Boot Image. Error message: $($_.Exception.Message)"
+                                Write-Warning -Message "An error occurred while configuring input locale for Boot Image: $($BootImagePath). Error message: $($_.Exception.Message)"
                             }
 
                             # Dismount boot image
+                            Write-Verbose -Message "Attempting to dismount '$($BootImagePackage.Name)' from: $($MountPath)"
                             $ReturnValue = Invoke-Executable -FilePath $DeploymentToolsDISMPath -Arguments "/Unmount-Image /MountDir:""$($MountPath)"" /Commit"
                             if ($ReturnValue -eq 0) {
                                 Write-Verbose -Message "Successfully dismounted the Boot Image from mount directory"
