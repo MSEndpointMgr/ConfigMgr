e059732727b655cdbb0ff18b532b7abd15dd8cd2
maurice-daly <maurice.daly@outlook.com>
2018-01-31 21:02:44 +0000

    Invoke-CMApplyDriverPackage - Version 2.0.5

--- Operating System Deployment/Drivers/Invoke-CMApplyDriverPackage.ps1
+++ Operating System Deployment/Drivers/Invoke-CMApplyDriverPackage.ps1
@@ -112,7 +112,7 @@ Process {
 		"OSUpgrade" {
 			$LogsDirectory = Join-Path $env:SystemRoot "Temp"
 		}
-		"DriveUpdate" {
+		"DriverUpdate" {
 			$LogsDirectory = Join-Path $env:SystemRoot "Temp"
 		}
 		default {
