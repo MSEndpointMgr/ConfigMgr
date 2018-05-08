0831f94304e0b2216591b849882df163fde61964
maurice-daly <maurice.daly@outlook.com>
2018-05-08 16:44:10 +0100

    Invoke-LenovoBIOSUpdate.ps1
    Version 1.0.5

--- Operating System Deployment/BIOS/Invoke-LenovoBIOSUpdate.ps1
+++ Operating System Deployment/BIOS/Invoke-LenovoBIOSUpdate.ps1
@@ -16,7 +16,7 @@
     Author:      Maurice Daly
     Contact:     @modaly_it
     Created:     2017-06-09
-    Updated:     2018-05-07
+    Updated:     2018-05-08
     
     Version history:
     1.0.0 - (2017-06-09) Script created
@@ -24,6 +24,7 @@
 	1.0.2 - (2018-01-29) Changed condition for the password switches
 	1.0.3 - (2018-04-30) Example conditional variable example updated. No functional changes
 	1.0.4 - (2018-05-07) Updated to copy in required OLEDLG.dll where missing in the BIOS package
+	1.0.5 - (2018-05-08) Updated to cater for varying OS source directory paths
 
 #>
 [CmdletBinding(SupportsShouldProcess = $true)]
@@ -38,22 +39,18 @@ param (
 	[ValidateNotNullOrEmpty()]
 	[string]$LogFileName = "LenovoFlashBiosUpdate.log"
 )
-Begin
-{
+Begin {
 	# Load Microsoft.SMS.TSEnvironment COM object
-	try
-	{
+	try {
 		$TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
 	}
 	catch [System.Exception] {
 		Write-Warning -Message "Unable to construct Microsoft.SMS.TSEnvironment object"
 	}
 }
-Process
-{
+Process {
 	# Functions
-	function Write-CMLogEntry
-	{
+	function Write-CMLogEntry {
 		param (
 			[parameter(Mandatory = $true, HelpMessage = "Value added to the log file.")]
 			[ValidateNotNullOrEmpty()]
@@ -82,60 +79,71 @@ Process
 		$LogText = "<![LOG[$($Value)]LOG]!><time=""$($Time)"" date=""$($Date)"" component=""LenovoBIOSUpdate.log"" context=""$($Context)"" type=""$($Severity)"" thread=""$($PID)"" file="""">"
 		
 		# Add value to log file
-		try
-		{
-			Out-File -InputObject $LogText -Append -NoClobber -Encoding Default -FilePath $LogFilePath -ErrorAction Stop 
+		try {
+			Out-File -InputObject $LogText -Append -NoClobber -Encoding Default -FilePath $LogFilePath -ErrorAction Stop
 		}
 		catch [System.Exception] {
 			Write-Warning -Message "Unable to append log entry to Invoke-LenovoBIOSUpdate.log file. Error message: $($_.Exception.Message)"
 		}
 	}
 	
-    Set-Location -Path $Path
+	Set-Location -Path $Path
 	# Write log file for script execution
 	Write-CMLogEntry -Value "Initiating script to determine flashing capabilities for Lenovo BIOS updates" -Severity 1
 	
 	# Check for required DLL's
-	if ((Test-Path -Path (Join-Path -Path $Path -ChildPath "OLEDLG.dll")) -eq $False){
-		Copy-Item -Path (Join-Path -path $TSEnvironment.Value("OSDisk") -ChildPath "Windows\System32\OLEDLG.dll") -Destination "$Path\OLEDLG.dll"
+	if ((Test-Path -Path (Join-Path -Path $Path -ChildPath "OLEDLG.dll")) -eq $False) {
+		Write-CMLogEntry -Value "Copying OLEDLG.dll to $Path directory" -Severity 1
+		if (([string]::IsNullOrEmpty($TSEnvironment.Value("OSDisk"))) -eq $false) {
+			Copy-Item -Path (Join-Path -path $TSEnvironment.Value("OSDisk") -ChildPath "Windows\System32\OLEDLG.dll") -Destination "$Path\OLEDLG.dll"
+		}
+		elseif ((Test-Path -Path C:\Windows\System32\OLEDLG.dll) -eq $true) {
+			Copy-Item -Path "C:\Windows\System32\OLEDLG.dll" -Destination "$Path\OLEDLG.dll"
+		}
+		elseif ((Test-Path -Path D:\Windows\System32\OLEDLG.dll) -eq $true) {
+			Copy-Item -Path "D:\Windows\System32\OLEDLG.dll" -Destination "$Path\OLEDLG.dll"
+		}
+		else {
+			Write-CMLogEntry -Value "Failed to copy DLL file. Abording update process" -Severity 3; exit 1
+		}
 	}
 	
 	# WinUPTP bios upgrade utility file name
-	if (([Environment]::Is64BitOperatingSystem) -eq $true)
-	{
-		$WinUPTPUtility = Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | Where-Object { $_.Name -like "WinUPTP64.exe" } | Select-Object -ExpandProperty FullName
+	if (([Environment]::Is64BitOperatingSystem) -eq $true) {
+		$WinUPTPUtility = Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | Where-Object {
+			$_.Name -like "WinUPTP64.exe"
+		} | Select-Object -ExpandProperty FullName
 	}
-	else
-	{
-		$WinUPTPUtility = Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | Where-Object { $_.Name -like "WinUPTP.exe" } | Select-Object -ExpandProperty FullName
+	else {
+		$WinUPTPUtility = Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | Where-Object {
+			$_.Name -like "WinUPTP.exe"
+		} | Select-Object -ExpandProperty FullName
 	}
 	
 	# Flash CMD upgrade utility file name
-	$FlashCMDUtility = Get-ChildItem -Path $Path -Filter "*.cmd" -Recurse | Where-Object { $_.Name -like "Flash.cmd" } | Select-Object -ExpandProperty FullName
+	$FlashCMDUtility = Get-ChildItem -Path $Path -Filter "*.cmd" -Recurse | Where-Object {
+		$_.Name -like "Flash.cmd"
+	} | Select-Object -ExpandProperty FullName
 	
-	if ($WinUPTPUtility -ne $null)
-	{
+	if ($WinUPTPUtility -ne $null) {
 		# Set required switches for silent upgrade of the bios and logging
 		Write-CMLogEntry -Value "Using WinUTPT BIOS update method" -Severity 1
 		$FlashSwitches = " /S"
 		$FlashUtility = $WinUPTPUtility
 	}
 	
-	if ($FlashCMDUtility -ne $null)
-	{
+	if ($FlashCMDUtility -ne $null) {
 		# Set required switches for silent upgrade of the bios and logging
 		Write-CMLogEntry -Value "Using FlashCMDUtility BIOS update method" -Severity 1
 		$FlashSwitches = " /quiet /sccm /ign"
 		$FlashUtility = $FlashCMDUtility
 	}
 	
-	if (!$FlashUtility)
-	{
+	if (!$FlashUtility) {
 		Write-CMLogEntry -Value "Supported upgrade utility was not found." -Severity 3
 	}
 	
-	if ($Password -ne $null)
-	{
+	if ($Password -ne $null) {
 		# Add password to the flash bios switches
 		$FlashSwitches = $FlashSwitches + " /pass:$Password"
 		Write-CMLogEntry -Value "Using the following switches for BIOS file: $($FlashSwitches -replace $Password, "<Password Removed>")" -Severity 1
@@ -144,44 +152,45 @@ Process
 	# Set log file location
 	$LogFilePath = Join-Path -Path $TSEnvironment.Value("_SMSTSLogPath") -ChildPath $LogFileName
 	
-	if (($TSEnvironment -ne $null) -and ($TSEnvironment.Value("_SMSTSinWinPE") -eq $true))
-	{		
-		try
-		{
+	if (($TSEnvironment -ne $null) -and ($TSEnvironment.Value("_SMSTSinWinPE") -eq $true)) {
+		try {
 			# Start flash update process
 			$FlashProcess = Start-Process -FilePath $FlashUtility -ArgumentList "$FlashSwitches" -Passthru -Wait
 			
 			#Output Exit Code for testing purposes
 			$FlashProcess.ExitCode | Out-File -FilePath $LogFilePath
-
-            #Get winuptp.log file
-            $winuptplog = Get-ChildItem -Filter "*.log" -Recurse | Where-Object { $_.Name -like "winuptp.log" } -ErrorAction SilentlyContinue| Select-Object -ExpandProperty FullName
-            Write-CMLogEntry -Value "winuptp.log file path is $($winuptplog)" -Severity 1
-            $smstslogpath = Join-Path -Path $TSEnvironment.Value("_SMSTSLogPath") -ChildPath "winuptp.log"			
-            Copy-Item -Path $winuptplog -Destination $smstslogpath -Force -ErrorAction SilentlyContinue
+			
+			#Get winuptp.log file
+			$winuptplog = Get-ChildItem -Filter "*.log" -Recurse | Where-Object {
+				$_.Name -like "winuptp.log"
+			} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+			Write-CMLogEntry -Value "winuptp.log file path is $($winuptplog)" -Severity 1
+			$smstslogpath = Join-Path -Path $TSEnvironment.Value("_SMSTSLogPath") -ChildPath "winuptp.log"
+			Copy-Item -Path $winuptplog -Destination $smstslogpath -Force -ErrorAction SilentlyContinue
 		}
 		catch [System.Exception] {
 			Write-CMLogEntry -Value "An error occured while updating the system BIOS in OS online phase. Error message: $($_.Exception.Message)" -Severity 3; exit 1
 		}
 	}
-	else
-	{
+	else {
 		# Used in a later section of the task sequence
 		
 		# Detect Bitlocker Status
-		$OSVolumeEncypted = if ((Manage-Bde -Status C:) -match "Protection On") { Write-Output $True }
-		else { Write-Output $False }
+		$OSVolumeEncypted = if ((Manage-Bde -Status C:) -match "Protection On") {
+			Write-Output $True
+		}
+		else {
+			Write-Output $False
+		}
 		
 		# Supend Bitlocker if $OSVolumeEncypted is $true
-		if ($OSVolumeEncypted -eq $true)
-		{
+		if ($OSVolumeEncypted -eq $true) {
 			Write-CMLogEntry -Value "Suspending BitLocker protected volume: C:" -Severity 1
 			Manage-Bde -Protectors -Disable C:
 		}
 		
 		# Start Bios update process
-		try
-		{
+		try {
 			Write-CMLogEntry -Value "Running Flash Update - $FlashUtility" -Severity 1
 			$FlashProcess = Start-Process -FilePath $FlashUtility -ArgumentList "$FlashSwitches" -Passthru -Wait
 			
