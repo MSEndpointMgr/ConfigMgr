e34ea1b909f6f538b7d3d2cc2ec8aead1f104865
Nickolaj Andersen <nickolaj@outlook.com>
2019-03-12 19:55:27 +0100

    Changed from cmdlet to dism.exe

--- Operating System Deployment/Invoke-RemoveBuiltinApps_1.0.0.ps1
+++ Operating System Deployment/Invoke-RemoveBuiltinApps_1.0.0.ps1
@@ -111,6 +111,42 @@ Process {
         }
     }
 
+	function Invoke-Executable {
+		param (
+			[parameter(Mandatory = $true, HelpMessage = "Specify the file name or path of the executable to be invoked, including the extension")]
+			[ValidateNotNullOrEmpty()]
+			[string]$FilePath,
+			[parameter(Mandatory = $false, HelpMessage = "Specify arguments that will be passed to the executable")]
+			[ValidateNotNull()]
+			[string]$Arguments
+		)
+		
+		# Construct a hash-table for default parameter splatting
+		$SplatArgs = @{
+			FilePath	 = $FilePath
+			NoNewWindow  = $true
+			Passthru	 = $true
+			ErrorAction  = "Stop"
+		}
+		
+		# Add ArgumentList param if present
+		if (-not ([System.String]::IsNullOrEmpty($Arguments))) {
+			$SplatArgs.Add("ArgumentList", $Arguments)
+		}
+		
+		# Invoke executable and wait for process to exit
+		try {
+			$Invocation = Start-Process @SplatArgs
+			$Handle = $Invocation.Handle
+			$Invocation.WaitForExit()
+		}
+		catch [System.Exception] {
+			Write-Warning -Message $_.Exception.Message; break
+		}
+		
+		return $Invocation.ExitCode
+	}    
+
     # Initial logging
     Write-LogEntry -Value "Starting built-in AppxPackage, AppxProvisioningPackage and Feature on Demand V2 removal process"
 
@@ -187,7 +223,14 @@ Process {
                     #
 
                     Write-LogEntry -Value "Removing AppxProvisioningPackage: $($AppProvisioningPackageName)"
-                    Remove-AppxProvisionedPackage -PackageName $AppProvisioningPackageName -Online -ErrorAction Stop | Out-Null
+                    #Remove-AppxProvisionedPackage -PackageName $AppProvisioningPackageName -Online -ErrorAction Stop | Out-Null
+                    $Invocation = Invoke-Executable -FilePath "dism.exe" -Arguments "/Online /Remove-ProvisionedAppxPackage /PackageName:$($AppProvisioningPackageName)"
+                    if ($Invocation -eq 0) {
+                        Write-LogEntry -Value "Successfully removed AppxProvisioningPackage: $($AppProvisioningPackageName)"
+                    }
+                    else {
+                        Write-LogEntry -Value "Failed to remove AppxProvisioningPackage, exit code $($Invocation) returned from process"
+                    }
                 }
                 catch [System.Exception] {
                     Write-LogEntry -Value "Removing AppxProvisioningPackage '$($AppProvisioningPackageName)' failed: $($_.Exception.Message)"
