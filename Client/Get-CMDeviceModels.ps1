7ef1aeb159f08e04ada9dab9fee4f6e9512f08aa
Nickolaj Andersen <nickolaj@outlook.com>
2019-03-12 09:34:22 +0100

    Get-CMDeviceModels version 1.0.1

--- Client/Get-CMDeviceModels.ps1
+++ Client/Get-CMDeviceModels.ps1
@@ -1,18 +1,27 @@
 ﻿<#
 .SYNOPSIS
-    Get all device models present in ConfigMgr 2012
+    Get all device models present in ConfigMgr
+
 .DESCRIPTION
-    This script will get all device models present in ConfigMgr 2012. It requires Hardware Inventory to be enabled and that the devices have reported a full hardware inventory report at least once.
+    This script will get all device models present in ConfigMgr. It requires Hardware Inventory to be enabled and that the devices have reported a full hardware inventory report at least once.
+
 .PARAMETER SiteServer
-    Site server name with SMS Provider installed
+    Site server name with SMS Provider installed.
+
 .EXAMPLE
+    # Get all device models on a Primary Site server called 'CM01':
     .\Get-CMDeviceModels.ps1 -SiteServer CM01
-    Get all device models on a Primary Site server called 'CM01':
+    
 .NOTES
-    Script name: Get-CMDeviceModels.ps1
+    FileName:    Get-CMDeviceModels.ps1
     Author:      Nickolaj Andersen
     Contact:     @NickolajA
-    DateCreated: 2015-04-10
+    Created:     2015-04-10
+    Updated:     2019-03-12
+
+    Version history:
+    1.0.0 - (2015-04-10) Script created
+    1.0.1 - (2019-03-12) Added manufacturer as a column in the output
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
 param(
@@ -40,23 +49,21 @@ Begin {
 Process {
     # ArrayList to store the models in
     $ModelsArrayList = New-Object -TypeName System.Collections.ArrayList
+
     # Enumerate through all models
-    $Models = Get-WmiObject -Namespace "root\SMS\site_$($SiteCode)" -Class SMS_G_System_COMPUTER_SYSTEM | Select-Object -Property Model
+    $ComputerSystems = Get-WmiObject -Namespace "root\SMS\site_$($SiteCode)" -Class SMS_G_System_COMPUTER_SYSTEM -ComputerName $SiteServer | Select-Object -Property Model, Manufacturer
+    
     # Add model to ArrayList if not present
-    if ($Models -ne $null) {
-        foreach ($Model in $Models) {
-            if ($Model.Model -notin $ModelsArrayList) {
-                $ModelsArrayList.Add($Model.Model) | Out-Null
+    if ($ComputerSystems -ne $null) {
+        foreach ($ComputerSystem in $ComputerSystems) {
+            if ($ComputerSystem.Model -notin $ModelsArrayList.Model) {
+                $PSObject = [PSCustomObject]@{
+                    Model = $ComputerSystem.Model
+                    Manufacturer = $ComputerSystem.Manufacturer
+                }
+                $ModelsArrayList.Add($PSObject) | Out-Null
             }
         }
     }
-    # Output the members of the ArrayList
-    if ($ModelsArrayList.Count -ge 1) {
-        foreach ($ModelItem in $ModelsArrayList) {
-            $PSObject = [PSCustomObject]@{
-                Model = $ModelItem
-            }
-            Write-Output $PSObject
-        }
-    }
+    Write-Output $ModelsArrayList
 }
