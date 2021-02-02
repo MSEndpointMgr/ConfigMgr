97d610da6986d374c278ff65acd9086442784333
Nickolaj Andersen <nickolaj@outlook.com>
2021-02-02 22:58:44 +0100

    1.2.0

--- Operating System Deployment/Invoke-RemoveBuiltinApps.ps1
+++ Operating System Deployment/Invoke-RemoveBuiltinApps.ps1
@@ -17,17 +17,18 @@
     Author:      Nickolaj Andersen
     Contact:     @NickolajA
     Created:     2019-03-10
-    Updated:     2019-10-03
+    Updated:     2021-02-02
 
     Version history:
     1.0.0 - (2019-03-10) Initial script updated with help section and a fix for randomly freezing
     1.1.0 - (2019-05-03) Added support for Windows 10 version 1903 (19H1)
     1.1.1 - (2019-08-13) Removed the part where it was disabling/enabling configuration for Store updates, as it's not needed
     1.1.2 - (2019-10-03) Removed unnecessary left over functions and updated catch statements so that they actually log the current app that could not be removed
+    1.2.0 - (2021-02-02) Added support for Windows 10 version 2004 (20H1) and 20H2
 #>
 Begin {
     # White list of Features On Demand V2 packages
-    $WhiteListOnDemand = "NetFX3|Tools.Graphics.DirectX|Tools.DeveloperMode.Core|Language|Browser.InternetExplorer|ContactSupport|OneCoreUAP|Media.WindowsMediaPlayer|Hello.Face"
+    $WhiteListOnDemand = "NetFX3|DirectX|Tools.DeveloperMode.Core|Language|InternetExplorer|ContactSupport|OneCoreUAP|WindowsMediaPlayer|Hello.Face|Notepad|MSPaint|PowerShell.ISE|ShellComponents"
 
     # White list of appx packages to keep installed
     $WhiteListedApps = New-Object -TypeName System.Collections.ArrayList
@@ -58,6 +59,21 @@ Begin {
 
     # Windows 10 version 1903
     # No new apps
+
+    # Windows 10 version 1909
+    $WhiteListedApps.AddRange(@(
+        "Microsoft.Outlook.DesktopIntegrationServicess"
+    ))
+
+    # Windows 10 version 2004
+    $WhiteListedApps.AddRange(@(
+        "Microsoft.VCLibs.140.00"
+    ))
+
+    # Windows 10 version 20H2
+    $WhiteListedApps.AddRange(@(
+        "Microsoft.MicrosoftEdge.Stable"
+    ))
 }
 Process {
     # Functions
@@ -140,10 +156,10 @@ Process {
 
         # Handle cmdlet limitations for older OS builds
         if ($OSBuildNumber -le "16299") {
-            $OnDemandFeatures = Get-WindowsCapability -Online -ErrorAction Stop | Where-Object { $_.Name -notmatch $WhiteListOnDemand -and $_.State -like "Installed"} | Select-Object -ExpandProperty Name
+            $OnDemandFeatures = Get-WindowsCapability -Online -ErrorAction Stop | Where-Object { $_.Name -notmatch $WhiteListOnDemand -and $_.State -like "Installed" } | Select-Object -ExpandProperty Name
         }
         else {
-            $OnDemandFeatures = Get-WindowsCapability -Online -LimitAccess -ErrorAction Stop | Where-Object { $_.Name -notmatch $WhiteListOnDemand -and $_.State -like "Installed"} | Select-Object -ExpandProperty Name
+            $OnDemandFeatures = Get-WindowsCapability -Online -LimitAccess -ErrorAction Stop | Where-Object { $_.Name -notmatch $WhiteListOnDemand -and $_.State -like "Installed" } | Select-Object -ExpandProperty Name
         }
 
         foreach ($Feature in $OnDemandFeatures) {
